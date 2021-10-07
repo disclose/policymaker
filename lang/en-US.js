f4c3fb55d488f7e42310372fef7ab1168b9a444c
Jeremy Manoto <jeremy@manoto.com.au>
2021-10-08 01:59:38 +1100

    Update language file to index en-US

--- lang/en-US.js
+++ lang/en-US.js
@@ -1,41 +1,41 @@
 export default async (context, locale) => {
-    return await Promise.resolve({
-        welcome: 'Welcome',
-        language: {
-            en: 'English',
-            ar: 'Arabic'
-        },
-        country: {
-            "*": "Global (All regions)",
-            AR: 'Arabic',
-            AU: 'Australia',
-            CA: 'Canada',
-            GB: 'Great Britain (UK)',
-            US: 'Unites States',
-            NZ: 'New Zealand',
-            BE: 'Belgium',
-            NL: 'Netherlands'
-        },
-        locale: {
-            "en-US": "English",
-            "ar-AR": "Arabic"
-        },  
+  return await Promise.resolve({
+    welcome: 'Welcome',
+    language: {
+      "en-US": 'English',
+      ar: 'Arabic'
+    },
+    country: {
+      "*": "Global (All regions)",
+      AR: 'Arabic',
+      AU: 'Australia',
+      CA: 'Canada',
+      GB: 'Great Britain (UK)',
+      US: 'Unites States',
+      NZ: 'New Zealand',
+      BE: 'Belgium',
+      NL: 'Netherlands'
+    },
+    locale: {
+      "en-US": "English",
+      "ar-AR": "Arabic"
+    },
 
-        policymaker: {
-            nav: {
-                introduction: 'Introduction',
-                organization_details: 'Organization details',
-                policy_settings: 'Policy settings',
-                download: 'Download'
-            },
+    policymaker: {
+      nav: {
+        introduction: 'Introduction',
+        organization_details: 'Organization details',
+        policy_settings: 'Policy settings',
+        download: 'Download'
+      },
 
-            organization_details: {
-                page_title: 'Organization details',
-                organization_name_label: 'What is the name of your organization?',
-                organization_name_desc: 'The organization that this policy will apply to',
-                organization_name_placeholder: 'Organization name',
-                organization_name_required: 'required'
-            }
-        }
-    })
+      organization_details: {
+        page_title: 'Organization details',
+        organization_name_label: 'What is the name of your organization?',
+        organization_name_desc: 'The organization that this policy will apply to',
+        organization_name_placeholder: 'Organization name',
+        organization_name_required: 'required'
+      }
+    }
+  })
 }
