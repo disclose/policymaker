6ef1cfdfd7e6a2f928518f9eef96a63cac0c4d0a
jmanoto <jeremy@manoto.com.au>
2022-08-08 03:08:14 +0000

    Deploying to gh-pages from @ disclose/policymaker@b134b02596f6e18a125b8fadbefe4981f42220fb 🚀

---
+++ _nuxt/static/1659928091/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1659928091",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"7bfeb1dc"}}}}(null,"\u002F"));
