f4c8ac7b0e06b849b740fe6237b028c2e9b7d34d
yesnet0 <12779999+yesnet0@users.noreply.github.com>
2026-07-05 09:07:42 +0000

    Deploying to gh-pages from @ disclose/policymaker@02cab73f0b9dc02c336851f8a4333b45c130cc50 🚀

--- _nuxt/static/1783242325/policymaker/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1783242325",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fpolicymaker",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cc8de8b6"}}}}(null));
