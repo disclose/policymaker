75f63963cadbcac7922606579d889f17f60fc7ec
yesnet0 <12779999+yesnet0@users.noreply.github.com>
2026-07-08 15:03:32 +0000

    Deploying to gh-pages from @ disclose/policymaker@9ac1a708c700ae80262cfe87f0a26f20ba90e40d 🚀

--- _nuxt/static/1783347127/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1783347127",layout:"default",error:a,state:a,serverRendered:false,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"4963d2b5"}}}}(null,"\u002F"));
